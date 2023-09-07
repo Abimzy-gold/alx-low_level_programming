4f76a9a4c4e2a79c66b869ab4f173dadf6f39b79
Abimzy-gold <abimzygold01@gmail.com>
2023-09-07 10:46:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - This is the program that describes the main function of the code
  *
- * Return:  Always  0 (Success)
+ * Return:  0 (Success)
  */
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c))
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
