6b250714ca545c9d5b700533910b296c2fc29375
Abimzy-gold <abimzygold01@gmail.com>
2023-09-12 11:13:39 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabest in lower case
+ * Return: void
+*/
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+	_putchar(c);
+
+	}
+	_putchar('\n');
+
+
+}
+
+
